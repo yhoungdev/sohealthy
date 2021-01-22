a069fe1035c704706ccc345b5fd04ed17c6b534c
yhoungdev <yhoungdev@gmail.com>
2021-01-22 22:05:13 +0000

    making the chatbot

--- src/components/bot.js
+++ src/components/bot.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import { Container , Form , Row , Col , Button , Badge } from 'reactstrap';
+import { Container , Form , Row , Col , Button , Badge , Input} from 'reactstrap';
 import './css/main.css';
 import { BiMenuAltLeft } from "react-icons/bi";
 
 
 const Bot=()=>{
+    //function for the bot 
+
+    const user=()=> {
+        //get the user reply 
+        var action;
+        const { user , reply ,inbox ,btn,app,res }={
+            user:document.querySelector('.userMsg'),
+            reply:document.querySelector('.reply'),
+            inbox:document.querySelector('.inbox'),
+            btn:document.querySelector('button'),
+            app:document.querySelector('.msgs'),
+            res:document.querySelector('.res')
+        }
+        
+
+        // the bot reply 
+        const botReply=(bt)=>{
+           let hello=document.createElement('p');
+           let wr=document.createTextNode('ooh my butiful lova this song na you i sing am for i cant explain how i feel deep inside ooh my love is real my beautiful gitl ')
+            hello.appendChild(wr)
+            app.appendChild(hello)
+            hello.classList.add('res')
+          
+        }
+        
+        //human msg
+        btn.onclick= e=> {
+            let make=document.createElement('div')
+            let write=document.createTextNode(inbox.value);
+            make.appendChild(write)
+            //now append the msg to the div
+            app.appendChild(make)
+
+            make.classList.add('mess')
+            botReply()
+        
+           
+        }
+        
+       
+        
+    }
     return (
         <>
             <Row>
@@ -31,8 +73,20 @@ const Bot=()=>{
                         <div className="botMsg">
                             <small>hello i am soHealthy , your health chatbot</small>
                         </div>
+                            <div>
+                                    <div className="msgs">
+                                </div>
+                                <div className="res">
+                                </div>
+                            </div>
 
 
+                        <footer>
+                            <Form onSubmit={e=>e.preventDefault()}>
+                                <Input className="inbox"/>          
+                                <button onClick={user}>click</button>               
+                              </Form>
+                        </footer>
 
                     </Container>
 
--- src/components/parent.jsx
+++ src/components/parent.jsx
@@ -1,21 +1,104 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './css/main.css';
-import { Container , Row , Col , Buuton  , Badge , Card ,CardText, CardFooter , CardTitle , CardBody} from 'reactstrap'
+import { Container , Row , Col , Button  , Badge , Card ,CardText, CardFooter , CardTitle , CardBody} from 'reactstrap'
 import { Link } from 'react-router-dom';
 import { HiChatAlt2 } from "react-icons/hi";
 import { MdLocalHospital } from "react-icons/md";
+import { BiMenuAltLeft } from "react-icons/bi";
+import { FaUserNurse } from "react-icons/fa";
+import {MdChatBubble} from "react-icons/md"
+
 const Main_page=()=>{
+  //get the element of sidebar and show it 
+    let [open , setOpen]=useState('none')
+    //now mute the sidebar
+    const close=()=>{
+      document.querySelector('.sidebar').style.display='none'
+      setOpen('none')
+    }
+
+    
+
     return(
-        <>
+        <div className="parent">
           <Row>
-             <Col className="col-12 col-md-4 col-lg-4">
-                left
+             <Col className="col-12 col-md-4 col-lg-4 first">
+                <Container>
+                    <h4 className="text-center">FEATURES</h4>
+                  <Row>
+                     <Col className="col-12 ">
+                        <Card className="features card-1">
+                        <div>
+                            <FaUserNurse/>
+                            <small className="ml-2 titleName">
+                              CONNECT WITH S DOCTOR
+                            </small>
+                            <br/>
+                            <small>
+                               You can connect with a doctor , to make your complains 
+                            </small>
+
+                        </div>
+                        </Card>
+
+                        
+                    </Col>
+                          {/* other col */}
+                     <Col className="col-12 mt-3">
+                        <Card className="features card-2">
+                            <div>
+                               <MdChatBubble/>
+                               <small className="titleName ml-2">
+                                    CHAT WITH HEALTH BOT
+                                 </small>  
+                                 <br/>
+                                 <small className="ml-2">
+                                    You can socialise with our health bot to learn more health tips 
+                                 </small>
+                            </div>
+                         
+
+                        </Card> 
+
+                     </Col>
+                      {/* third col */}
+                     <Col className="col-12 mt-3 last">
+                        <Card className="features card-2">
+                            <div>
+                               <MdChatBubble/>
+                               <small className="titleName ml-2">
+                                    CHAT WITH HEALTH BOT
+                                 </small>  
+                                 <br/>
+                                 <small className="ml-2">
+                                    You can socialise with our health bot to learn more health tips 
+                                 </small>
+                            </div>
+                         
+
+                        </Card> 
+                     </Col>
+                  </Row>
+
+
+                </Container>
+
             </Col>
             {/* second row */}
              <Col className="col-12 col-md-4 col-lg-4">
                 <div className="middles">
+                  <div className="sidebar" style={{display:open}}>
+                    <h3 onClick={close}>&times;</h3>
+                     <ul>
+                      
+                       <li> Meet</li>
+                       <li> Connect </li>
+                       <li> Chat </li>
+                       <li> About</li>
+                     </ul>
+                  </div>
                   <header>
-                    header part
+                      <h6 onClick={()=>setOpen('block')}><BiMenuAltLeft/></h6>
                   </header>
                   <Container className="mt-5">
                       <Row>
@@ -27,8 +110,10 @@ const Main_page=()=>{
                                   <span className="emo">
                                    <HiChatAlt2/>
                                   </span>
+                                  <br/>
+                                  <small className="text-muted">Chat with bot</small>
                                 </CardTitle>
-                                <CardText><small className="text-muted">Chat with bot</small></CardText>
+                               
                               </CardBody>
                             </Link>
                         </Card>
@@ -41,9 +126,10 @@ const Main_page=()=>{
                             <CardTitle>
                                 <span className="emo"> 
                                   <MdLocalHospital/>
-                                </span>
+                                </span> <br/>
+                                <small className="text-muted">Connect with a Doctor </small>
                             </CardTitle>
-                            <CardText>connect with a Doctor </CardText>
+                        
                           </CardBody>
                         </Card>
                         </Col>
@@ -73,7 +159,7 @@ const Main_page=()=>{
           </Row>
           
 
-        </>
+        </div>
     )
 }
 export default Main_page;
